023000c75bf7cd92f1a5ef594aa0c4567845981e
Giovanny Santos <giovannysantos297@gmail.com>
2025-06-26 17:53:25 -0300

    Adicionar arquivos de projeto.

---
+++ src/CadastroClientes/CadastroClientes.API/Controllers/ClientesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CadastroClientes.API.Controllers
+{
+    public class ClientesController : Controller
+    {
+        [HttpPost("/[controller]")]
+        public IActionResult Cadastrar()
+        {
+            return Ok();
+        }
+        
+        [HttpGet("/[controller]")]
+        public IActionResult Obter(Guid idCliente)
+        {
+            return Ok();
+        }
+    }
+}
