0dcf7251cbbaaa8d789cfbbd94023ee3b251e17a
Giovanny Santos <giovannysantos297@gmail.com>
2025-06-27 20:57:07 -0300

    Configurações e ajustes

--- src/CadastroClientes/CadastroClientes.Infra/Migrations/20250627230858_MigrationPostgres.Designer.cs
+++ src/CadastroClientes/CadastroClientes.Infra/Migrations/20250627230858_MigrationPostgres.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CadastroClientes.Infra.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CadastroClientes.Infra.Migrations
 {
     [DbContext(typeof(CadastroClientesDbContext))]
-    partial class CadastroClientesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250627230858_MigrationPostgres")]
+    partial class MigrationPostgres
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
